4b067077b0a26d559efc3271639a520ba3ada45e
DawalaGH <dawalaamidudar@gmail.com>
2025-02-02 22:28:08 +0000

    first commit

---
+++ Average.py
@@ -0,0 +1,14 @@
+#storing the values
+animal1=10
+animal2=60
+animal3=90
+animal4=100
+animal5=20
+
+#find sum of trees
+sum=animal1+animal2+animal3+animal4+animal5
+print("Total number of trees:",sum)
+
+#average
+average=sum/5
+print("the average is:",average)
